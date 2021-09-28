5718be9e9b8dfea695dd8ed7669873104e2202bb
Chris323 <chrisortega94@gmail.com>
2021-09-28 13:45:17 -0700

    Add files via upload

---
+++ NewTryForLearning/NewTryForLearning/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NewTryForLearning
+{
+    //Name does NOT have to match file, class name can be different
+    class Program
+    {
+        //C# has many main signatures, not just the one like java as seen below
+        //public static main (String[] args){} 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello there!");
+        }
+    }
+}
